bc575afb751c35b5bd0333a67d5444abb186f27c
sawaisinghbusiness <sawaisinghbusiness@gmail.com>
2026-07-31 14:54:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -133,4 +133,4 @@ app.post("/verify-otp", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
- 
+ });
